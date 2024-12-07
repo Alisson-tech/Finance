48ae00c783cf5f23476845d784eb09fc06d5f624
aliss <aliss@DESKTOP-QO9AS8C>
2024-12-06 23:07:37 -0300

    Create Controller

---
+++ Controllers/ExpensiveTypeController.cs
@@ -0,0 +1,59 @@
+﻿using Finance.Service.Revenue;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Finance.Controllers;
+
+[Controller]
+public class ExpensiveTypeController : Controller
+{
+    public readonly IExpensiveTypeService _expensiveTypeService;
+
+    public ExpensiveTypeController(IExpensiveTypeService expensiveTypeService)
+    {
+        _expensiveTypeService = expensiveTypeService;
+    }
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetById(int id)
+    {
+        var expensiveType = await _expensiveTypeService.GetById(id);
+
+        if (expensiveType == null)
+        {
+            return NotFound();
+        }
+
+        return Ok(expensiveType);
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAll()
+    {
+        return Ok(await _expensiveTypeService.GetAll());
+ 
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Create([FromBody] ExpensiveTypeDto expensiveTypeDto)
+    {
+        if (!ModelState.IsValid)
+        {
+            return BadRequest(ModelState);
+        }
+
+        var createdExpensiveType = await _expensiveTypeService.Create(expensiveTypeDto);
+        return CreatedAtAction(nameof(GetById), new { id = createdExpensiveType.Id }, createdExpensiveType);
+    }
+
+    [HttpPut("{id}")]
+    public async Task<IActionResult> Update( [FromBody] ExpensiveTypeDto expensiveTypeDto)
+    {
+        if (!ModelState.IsValid)
+        {
+            return BadRequest(ModelState);
+        }
+
+        var updatedExpensiveType = await _expensiveTypeService.Update(expensiveTypeDto);
+        return Ok(updatedExpensiveType);
+    }
+}
